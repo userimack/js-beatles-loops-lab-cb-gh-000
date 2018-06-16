5ee03986ce27bfbb883d6af9ec0ba7d593b47988
Mahendra Yadav <mahendra.k12@gmail.com>
2018-06-16 18:48:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = new Array();
+  for (var i=0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var array = new Array();
+  for(var i=0; i<facts.length; i++){
+    array.push(`${facts[i]}!!!`)
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var array = new Array();
+  do {
+    array.push("I love the Beatles!");
+    number += 1;
+  } while(number < 15)
+  return  array
+}
